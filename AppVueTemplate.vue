ef4e1c101a9cbfc15d15703e20ecc65bb787a685
msr <mshineray@qq.com>
2021-09-26 14:27:22 +0800

    init AppVueTemplate.vue

---
+++ AppVueTemplate.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'AppVueTemplate',
+  components: {
+  },
+  props:{
+
+  },
+  mixins: [],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  created(){
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+
+}
+</style>
